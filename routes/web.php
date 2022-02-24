cc1463a596634059e99375275c8739edb8fed367
Rosandymaulana <rosandy1118@gmail.com>
2022-02-24 21:50:40 +0700

    Update Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,53 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>EducaStudio<br></h1>";
+    echo "Halaman Awal";
 });
+
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        echo "<h1>Marbel-Edu-Games</h1>";
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        echo "<h1>marbel-and-friends-kids-games</h1>";
+    });
+    Route::get('/riri-story-books', function () {
+        echo "<h1>riri-story-books</h1>";
+    });
+    Route::get('/kolak-kids-songs', function () {
+        echo "<h1>kolak-kids-songs</h1>";
+    });
+});
+
+Route::get('/news/{id}', function ($id) {
+    return 'Ini berita news dengan nomor ' . $id;
+});
+
+Route::get('/news/{post}/comments/{comment}', function ($postId, $commentId) {
+    return "Ini route parameter" . $postId . $commentId;
+});
+
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        echo "Desainer<br>";
+        echo "Gamer";
+    });
+    Route::get('/magang', function () {
+        echo "Selamat Datang di menu magang";
+    });
+    Route::get('/kunjungan-industri', function () {
+        echo "Ini halaman kunjungan industri";
+    });
+});
+
+Route::get('/about-us', function () {
+    echo 'halaman about us<br>';
+    echo "Hello Semua";
+});
+
+
+Route::resources([
+    'contact' => ContactController::class,
+    'contact/create' => ContactController::class
+]);
